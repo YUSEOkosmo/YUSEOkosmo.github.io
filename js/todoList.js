9d00058498c82095dbbbced6606bfff3f576c966
YUSEOkosmo <hs671005@naver.com>
2023-06-25 16:16:14 +0900

    2222

--- js/todoList.js
+++ js/todoList.js
@@ -1,10 +1,56 @@
 const todoForm = document.getElementById("todoForm");
 const todoList = document.getElementById("todoList");
+const todoInput = todoForm.querySelector("input");
 
+let todos = [];
+
+const TODOS_KEY = "TODOs";
+
+function saveTODOs(){
+    localStorage.setItem(TODOS_KEY,JSON.stringify(todos));
+}
+function removeTODO(event){
+    const target_li = event.target.parentElement;
+    target_li.remove();
+}
+
+function writeTODOonPage(todo){
+    const li = document.createElement("li");
+    li.id = todo.id;
+    const span = document.createElement("span");
+    span.innerText = todo.text;
+
+    const button = document.createElement("button");
+    button.innerText = "❌Remove";
+    
+    button.addEventListener("click", removeTODO);
+    li.appendChild(span);
+    li.appendChild(button);
+    
+    todoList.appendChild(li);
+}
+
+const savedTODOs = localStorage.getItem(TODOS_KEY);
+
+if(savedTODOs !== null){
+    const parsedTODOs = JSON.parse(savedTODOs);
+    todos = parsedTODOs;
+    parsedTODOs.forEach(writeTODOonPage);
+    
+}
 
 function handleTodoForm(event){
     event.preventDefault();
+    const myTodo = todoInput.value;
+    todoInput.value = "";
+    const todoObject = {
+        text: myTodo,
+        id: Date.now()
+    };
+    todos.push(todoObject);
+    writeTODOonPage(todoObject);
+    saveTODOs();
 }
 
-
 todoForm.addEventListener("submit",handleTodoForm);
+
